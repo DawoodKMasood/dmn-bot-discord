1a9d13e382bf9f2d57b2f87c36a14069d722a000
Dawood Khan Masood <91176669+DawoodKMasood@users.noreply.github.com>
2023-01-03 11:05:21 +0500

    Update index.js

--- index.js
+++ index.js
@@ -775,7 +775,15 @@ function startWebsocket() {
 
                             // check if remaining expired time is greater than 35 hours
                             if (new Date(object.expired) - Date.now() > 126000000) {
-                                if (object.level === 3) {
+                                if (object.level === 1) {
+                                    // send cmine location to master account
+                                    sendLocation(object.level, object.code, CONTINENT, object.loc[1], object.loc[2]);
+                                }
+                            }
+
+                            // check if remaining expired time is greater than 35 hours
+                            if (new Date(object.expired) - Date.now() > 126000000) {
+                                if (object.level === 2) {
                                     // send cmine location to master account
                                     sendLocation(object.level, object.code, CONTINENT, object.loc[1], object.loc[2]);
                                 }
@@ -783,7 +791,7 @@ function startWebsocket() {
 
                             // check if remaining expired time is greater than 40 hours
                             if (new Date(object.expired) - Date.now() > 144000000) {
-                                if (object.level === 2) {
+                                if (object.level === 3) {
                                     // send cmine location to master account
                                     sendLocation(object.level, object.code, CONTINENT, object.loc[1], object.loc[2]);
                                 }
