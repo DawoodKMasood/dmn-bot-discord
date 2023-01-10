c26ac31672fa6203efa181033d63cc6a7a428f18
Dawood Khan Masood <91176669+DawoodKMasood@users.noreply.github.com>
2023-01-10 15:08:19 +0500

    Update index.js

--- index.js
+++ index.js
@@ -49,7 +49,10 @@ let startPosition = 4095;
 let endPosition = 0;
 let wsOpen = false;
 let ACCESS_TOKEN = '';
-let ACCOUNT_ID = process.env.ACCOUNT_ID;
+let ACCOUNT_ID_1 = '61e0484f0d165f2c1969fb5c';
+let ACCOUNT_ID_2 = '62790152cb68926e9ebf8365';
+let ACCOUNT_ID_3 = '62ef390a6d2d331428646220';
+let ACCOUNT_ID_4 = '62f5bb23eee8291859cc0883';
 let CONTINENT = process.env.CONTINENT;
 
 // get gold mine locations from user input and send to discord
@@ -664,7 +667,22 @@ async function sendLocation(level, objectCode, continent, x, y) {
     };
 
     axios.post('https://api-lok-live.leagueofkingdoms.com/api/chat/new',
-        `json=%7B%22chatChannel%22%3A3%2C%22chatType%22%3A2%2C%22text%22%3A%22Lv.${level}%3Ffo_${objectCode}%22%2C%22param%22%3A%7B%22loc%22%3A%5B${continent}%2C${x}%2C${y}%5D%7D%2C%22toId%22%3A%22${ACCOUNT_ID}%22%7D`,
+        `json=%7B%22chatChannel%22%3A3%2C%22chatType%22%3A2%2C%22text%22%3A%22Lv.${level}%3Ffo_${objectCode}%22%2C%22param%22%3A%7B%22loc%22%3A%5B${continent}%2C${x}%2C${y}%5D%7D%2C%22toId%22%3A%22${ACCOUNT_ID_1}%22%7D`,
+        { headers: headers }
+    )
+
+    axios.post('https://api-lok-live.leagueofkingdoms.com/api/chat/new',
+        `json=%7B%22chatChannel%22%3A3%2C%22chatType%22%3A2%2C%22text%22%3A%22Lv.${level}%3Ffo_${objectCode}%22%2C%22param%22%3A%7B%22loc%22%3A%5B${continent}%2C${x}%2C${y}%5D%7D%2C%22toId%22%3A%22${ACCOUNT_ID_2}%22%7D`,
+        { headers: headers }
+    )
+
+    axios.post('https://api-lok-live.leagueofkingdoms.com/api/chat/new',
+        `json=%7B%22chatChannel%22%3A3%2C%22chatType%22%3A2%2C%22text%22%3A%22Lv.${level}%3Ffo_${objectCode}%22%2C%22param%22%3A%7B%22loc%22%3A%5B${continent}%2C${x}%2C${y}%5D%7D%2C%22toId%22%3A%22${ACCOUNT_ID_3}%22%7D`,
+        { headers: headers }
+    )
+
+    axios.post('https://api-lok-live.leagueofkingdoms.com/api/chat/new',
+        `json=%7B%22chatChannel%22%3A3%2C%22chatType%22%3A2%2C%22text%22%3A%22Lv.${level}%3Ffo_${objectCode}%22%2C%22param%22%3A%7B%22loc%22%3A%5B${continent}%2C${x}%2C${y}%5D%7D%2C%22toId%22%3A%22${ACCOUNT_ID_4}%22%7D`,
         { headers: headers }
     )
 }
